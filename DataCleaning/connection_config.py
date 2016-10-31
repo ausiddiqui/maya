4cfa1358fb4e487977f2cb65666c20ec15fc4dd6
Salman Quaiyum <salman.quaiyum@jpgsph.edu>
2016-10-31 10:19:14 +0600

    Add MySQL config file

---
+++ DataCleaning/connection_config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 29 15:16:15 2016
+
+@author: salman.quaiyum
+"""
+
+hostname = "maya-ds-mysql-sg.ckzphzuffx4k.ap-southeast-1.rds.amazonaws.com"
+port = 3306
+db = "maya"
+usr =
+pwd = 
