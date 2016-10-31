2cb893e14f42d40dfb917962e64cb2353720864e
Salman Quaiyum <salman.quaiyum@jpgsph.edu>
2016-10-31 11:53:27 +0600

    Remove server information

--- DataCleaning/connection_config.py
+++ DataCleaning/connection_config.py
@@ -5,7 +5,7 @@ Created on Sat Oct 29 15:16:15 2016
 @author: salman.quaiyum
 """
 
-hostname = "maya-ds-mysql-sg.ckzphzuffx4k.ap-southeast-1.rds.amazonaws.com"
+hostname = 
 port = 3306
 db = "maya"
 usr =
